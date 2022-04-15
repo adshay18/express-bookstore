0326f6fe04cb3c36c8739d79bfcc1841c8429639
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-04-15 14:24:27 -0400

    clean up routes

--- config.js
+++ config.js
@@ -1,13 +1,21 @@
 /** Common config for bookstore. */
-
+const jsonschema = require('jsonschema');
+const ExpressError = require('../expressError');
 
 let DB_URI = `postgresql://`;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+if (process.env.NODE_ENV === 'test') {
+	DB_URI = `${DB_URI}/books-test`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+	DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
 }
 
+function validate(request, schema) {
+	const result = jsonschema.validate(request, schema);
+	if (!result.valid) {
+		let listOfErrors = result.errors.map((error) => error.stack);
+		throw new ExpressError(listOfErrors, 400);
+	}
+}
 
-module.exports = { DB_URI };
+module.exports = { DB_URI, validate };
